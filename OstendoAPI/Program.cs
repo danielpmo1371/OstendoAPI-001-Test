ba1e8f87ecb06a43e8618437edd70cd0899b4811
Daniel Paiva <danielpmo@gmail.com>
2018-01-09 21:55:55 +1300

    Added support to tabledata and type in api controllers, id and tableparam to consult.

--- OstendoAPI/Program.cs
+++ OstendoAPI/Program.cs
@@ -22,7 +22,7 @@ namespace OstendoAPI
             client.BaseAddress = new Uri("http://Ostendo.ddns.net:235");
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
-            Console.WriteLine("System initialized! Consuling API after you press Enter...");
+            Console.WriteLine("System initialized! I'll ask you what you want from the API after you press Enter...");
             Console.ReadLine();
             returned = await ReachAPI(client, returned);
             Console.WriteLine(returned);
@@ -31,7 +31,20 @@ namespace OstendoAPI
         }
         private static async Task<string> ReachAPI(HttpClient client, string returned)
         {
-            HttpResponseMessage response = await client.GetAsync("/assemblyorder/WO1563?apikey=Ytm1VIhM2ai7fewNDR1FuJ8cJx4OCPQOAD95Dn94ih4pM00ClrQXfFUAAbnlMFVmCkCo4ZbWoD48196cwaZyzT0pbln270loYHHjC2tQ7fz5sg%3D%3D&configuration=0");
+            //string controller = "assemblyorder";
+            //string id = "/WO1563";
+            string controller = "tabledata";
+            string id = "";
+            string tableParam = "&tablename=standardunits&format=xml";
+            Console.WriteLine("What controller do you want to use?");
+            controller = Console.ReadLine();
+            Console.WriteLine("What id do you want to use? If you want a table, leave it blank");
+            id = "/"+Console.ReadLine();
+            Console.WriteLine("If you want a table, what's the table name?");
+            tableParam = Console.ReadLine();
+            if (tableParam != "") { tableParam = "&tablename=" + tableParam + "&format=xml"; }
+            
+            HttpResponseMessage response = await client.GetAsync("/"+controller+id+"?apikey=Ytm1VIhM2ai7fewNDR1FuJ8cJx4OCPQOAD95Dn94ih4pM00ClrQXfFUAAbnlMFVmCkCo4ZbWoD48196cwaZyzT0pbln270loYHHjC2tQ7fz5sg%3D%3D&configuration=0"+tableParam);
             if (response.IsSuccessStatusCode)
             {
                 //product = await response.Content.ReadAsAsync<AssemblyIssue>();
@@ -41,7 +54,7 @@ namespace OstendoAPI
             }
             else
             {
-                Console.WriteLine("API Consulted and answered a non successfull code! Press Enter to see the result...");
+                Console.WriteLine("API Consulted and answered an ERROR code! Press Enter to see the result...");
 
             }
 
